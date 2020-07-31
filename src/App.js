35317839dbffe5a6da6eaf18e8e07fdc9cf2b944
sureshbabuvpk <sureshbabuweb@gmail.com>
2020-07-31 16:03:21 +0530

    name changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React awesome app for learning </p>
+        <p>React awesome app for learning sureshbabuweb.in</p>
       </header>
     </div>
   );
