6205d2465c02f5103d39a97b6e49d45f7a2f082b
sureshbabuvpk <sureshbabuweb@gmail.com>
2020-08-01 13:50:07 +0530

    Reactjs-tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React awesome app for learning sureshbabuweb.in</p>
-      </header>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold txt-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            200
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            400
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
