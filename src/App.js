067a17dc070dfb1ea02febc7fb8583b324f6ed3e
sureshbabuvpk <sureshbabuweb@gmail.com>
2020-08-02 09:19:22 +0530

    commit for search option

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/imageCard";
+import ImageSearch from "./components/imageSearch";
+
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -7,11 +9,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-<<<<<<< HEAD
-      //`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
-=======
-       // `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
->>>>>>> 1b9995bf7d67fb98cb3b4d0244d9a915bdcacf75
+      // `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
       `https://pixabay.com/api/?key=17730075-3b4e6b63ca48bfc279e04b805&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
@@ -23,11 +21,21 @@ function App() {
   }, [term]);
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="h1.text-5xl text-center mx-auto mt-32">
+          No Images Found
+        </h1>
+      )}
+      {isLoading ? (
+        <h1 className="h1.text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
